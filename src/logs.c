guillaumebour/yang

commit a2640eab80f0e83cdb9a03686e31b916a4e570e5

logs: Sanitize code according to the usual code conventions

src/logs.c
@@ -8,37 +8,32 @@ void log_error(char *msg, ...)
 {
     if(verbosity_level < 0)
     {
-        return;
+        va_list argptr;
+        va_start(argptr, msg);
+        fprintf(stderr, "\x1b[31m(╯°□°）╯︵ ┻━┻\x1b[0m: ");
+        vfprintf(stderr, msg, argptr);
+        fprintf(stderr, "\n");
+        va_end(argptr);
     }
-
-    va_list argptr;
-    va_start(argptr, msg);
-    fprintf(stderr, "\x1b[31m(╯°□°）╯︵ ┻━┻\x1b[0m: ");
-    vfprintf(stderr, msg, argptr);
-    fprintf(stderr, "\n");
-    va_end(argptr);
 }
 
 void log_warning(int verbosity, char *msg, ...)
 {
     if(verbosity_level < verbosity)
     {
-        return;
+        va_list argptr;
+        va_start(argptr, msg);
+        fprintf(stderr, "\x1b[33m¯\\_(ツ)_/¯\x1b[0m: ");
+        vfprintf(stderr, msg, argptr);
+        fprintf(stderr, "\n");
+        va_end(argptr);
     }
-
-    va_list argptr;
-    va_start(argptr, msg);
-    fprintf(stderr, "\x1b[33m¯\\_(ツ)_/¯\x1b[0m: ");
-    vfprintf(stderr, msg, argptr);
-    fprintf(stderr, "\n");
-    va_end(argptr);
 }
 
 void set_verbosity_level(int level)
 {
-    if(level >= -1 || level <= 3) {
+    if(level >= -1 || level <= 3)
         verbosity_level = level;
-    } else {
+    else
         verbosity_level = VERBOSITY_NORMAL;
-    }
 }

commit 5120ffde0451d9d182306a631252f732af506400

logs: Fix verbosity level check due to a regression in a2640eab80f0e83cdb9a03686e31b916a4e570e5

src/logs.c
@@ -6,7 +6,7 @@ static int verbosity_level;
 
 void log_error(char *msg, ...)
 {
-    if(verbosity_level < 0)
+    if(verbosity_level >= 0)
     {
         va_list argptr;
         va_start(argptr, msg);
@@ -19,7 +19,7 @@ void log_error(char *msg, ...)
 
 void log_warning(int verbosity, char *msg, ...)
 {
-    if(verbosity_level < verbosity)
+    if(verbosity_level >= verbosity)
     {
         va_list argptr;
         va_start(argptr, msg);